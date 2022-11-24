bc8745aefc1874d9b164098e13881116b19efcb0
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-11-23 21:51:56 -0300

    Remove padding

--- src/layouts/DefaultLayout/default-layout.styles.ts
+++ src/layouts/DefaultLayout/default-layout.styles.ts
@@ -4,7 +4,7 @@ export const LayoutContainer = styled.div`
   /* width: 1440px; */
   max-width: 70rem;
   margin: 0 auto;
-  padding: 0 1rem;
+  /* padding: 0 1rem; */
 
   /* @media (min-width: 1440px) {
     padding: 0 10rem;
