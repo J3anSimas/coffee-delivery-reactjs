b1d77f3b54a583404481d1b98441c5638c88177d
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-11-23 21:16:03 -0300

    Improving default layout

--- src/layouts/DefaultLayout/default-layout.styles.ts
+++ src/layouts/DefaultLayout/default-layout.styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   /* width: 1440px; */
-  max-width: 1440px;
+  max-width: 70rem;
   margin: 0 auto;
   padding: 0 1rem;
 
-  @media (min-width: 1440px) {
+  /* @media (min-width: 1440px) {
     padding: 0 10rem;
-  }
+  } */
 `
