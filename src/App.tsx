7dcf35709ad7bf26024546ba0b09a88b1d21acd3
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-11-24 22:14:25 -0300

    Adding Contexts to App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './contexts/cart.context'
+import { CoffeeItemListContextProvider } from './contexts/coffee-items-list.context'
 import Router from './router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
-function App (): JSX.Element {
+function App(): JSX.Element {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
+      <CartContextProvider>
+        <CoffeeItemListContextProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+          <GlobalStyle />
+        </CoffeeItemListContextProvider>
+      </CartContextProvider>
     </ThemeProvider>
   )
 }
