db0400c481827a8af7c16fe2055ffc5dbdb56126
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-11-24 22:17:54 -0300

    Creating cart context

---
+++ src/contexts/cart.context.tsx
@@ -0,0 +1,88 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+export type TCartItem = {
+  cartId: string
+  coffeeId: number
+  quantity: number
+}
+type TCartContext = {
+  cartItems: TCartItem[]
+  addItemToCart: ({
+    coffeeId,
+    quantity
+  }: {
+    coffeeId: number
+    quantity: number
+  }) => void
+  removeCartItemFromList: (cartId: string) => void
+  getCartItemFromId: (cartId: string) => TCartItem | undefined
+  amountCartItems: number
+}
+
+export const CartContext = createContext<TCartContext>({} as TCartContext)
+
+type TCartContextProviderProps = {
+  children: ReactNode
+}
+
+export function CartContextProvider({
+  children
+}: TCartContextProviderProps): JSX.Element {
+  const [cartItems, setCartItems] = useState<TCartItem[]>([])
+  const [amountCartItems, setAmountCartItems] = useState(0)
+
+  function addItemToCart({
+    coffeeId,
+    quantity
+  }: {
+    coffeeId: number
+    quantity: number
+  }): void {
+    setCartItems((state) => [
+      ...state,
+      { cartId: new Date().getTime().toString(), coffeeId, quantity }
+    ])
+    setAmountCartItems((state) => state + quantity)
+  }
+
+  function removeCartItemFromList(cartId: string): void {
+    const newList = cartItems.filter((cartItem) => cartItem.cartId !== cartId)
+    setCartItems(newList)
+  }
+
+  function getCartItemFromId(cartId: string): TCartItem | undefined {
+    const cartItem = cartItems.find((cartT) => cartT.cartId === cartId)
+    return cartItem
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addItemToCart,
+        amountCartItems,
+        getCartItemFromId,
+        removeCartItemFromList
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export function useCart(): TCartContext {
+  const {
+    addItemToCart,
+    amountCartItems,
+    cartItems,
+    getCartItemFromId,
+    removeCartItemFromList
+  } = useContext(CartContext)
+  return {
+    cartItems,
+    addItemToCart,
+    amountCartItems,
+    getCartItemFromId,
+    removeCartItemFromList
+  }
+}
